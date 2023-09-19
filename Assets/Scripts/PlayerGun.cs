af85f52e12cf4e1a53c84bed76f53d5734af907c
RawOffline <rawandi54@gmail.com>
2023-09-19 11:16:13 +0200

    något med pistolen

--- Assets/Scripts/PlayerGun.cs
+++ Assets/Scripts/PlayerGun.cs
@@ -9,11 +9,23 @@ public class PlayerGun : MonoBehaviour
     float timer;
     float fireRate = 0.1f;
 
+    //public Transform targetObject; // Reference to the GameObject whose left side will trigger the scale change.
+    //public float scaleValue = -1f;  // The scale value to set when the collision occurs.
+
     // Update is called once per frame
     void Update()
     {
         Vector2 direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         transform.right = direction;
+
+        // Check if the GameObject's x-coordinate is less than or equal to the targetObject's left side.
+        /*if (transform.position.x <= targetObject.position.x)
+        {
+            // Change the scale of the GameObject on the x-axis.
+            Vector3 newScale = transform.localScale;
+            newScale.x = scaleValue;
+            transform.localScale = newScale;
+        }*/
 
         if (Input.GetMouseButton(0) && timer > fireRate)
         {
