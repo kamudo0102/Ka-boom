6ecdd3ade10b994a542a1f4432629d6647265826
kamudo0102 <kamonphornudornrak@hotmail.com>
2023-09-19 15:23:26 +0200

    fix camera

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -1,18 +1,19 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject Player;
+    CinemachineVirtualCamera vcam;
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        vcam = GetComponent<CinemachineVirtualCamera>();
+
+        vcam.Follow = GameObject.FindWithTag("Player").transform;
+
+
     }
 }
