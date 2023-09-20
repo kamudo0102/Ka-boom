9027db6333079582a72a19de9a05a6fc558c8720
RawOffline <rawandi54@gmail.com>
2023-09-20 12:38:42 +0200

    Animation DownLeftWalk
    Bonden kan gå åt 1/4 håll nu... i hope.

--- Assets/Scripts/PlayerGun.cs
+++ Assets/Scripts/PlayerGun.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Player.Prefs.SetInt("Variabelnamn", värde)
+// Player.Prefs.GetInt("Variabelnamn")
 public class PlayerGun : MonoBehaviour
 {
     public GameObject player;
