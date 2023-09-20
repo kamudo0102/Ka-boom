0057bfd143c6162c7f24ee2afc75d67380db1bbb
kamudo0102 <kamonphornudornrak@hotmail.com>
2023-09-20 14:53:44 +0200

    pushar reload scene

--- Assets/Script/HealthAndDamageController.cs
+++ Assets/Script/HealthAndDamageController.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class HealthAndDamageController : MonoBehaviour
 {
     
     public float playerHealth;
     public float enemyDamage;
+    [SerializeField]public float screenDelayTime = 1f;
     [SerializeField]public Image[] hearts;
+
+   
 
     private CinemachineImpulseSource screenShaker;
     
@@ -18,6 +23,7 @@ public class HealthAndDamageController : MonoBehaviour
     {
         UpdateHealth();
         screenShaker = FindAnyObjectByType<CinemachineImpulseSource>();
+        
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -27,6 +33,10 @@ public class HealthAndDamageController : MonoBehaviour
             Damage();
 
             screenShaker.GenerateImpulse((collision.transform.position - transform.position).normalized * 0.1f);
+        }
+        if (playerHealth == 0)
+        {
+            ReloadScene();
         }
     }
 
@@ -41,22 +51,19 @@ public class HealthAndDamageController : MonoBehaviour
     // Update is called once per frame
     public void UpdateHealth()
     {
-        if (playerHealth <= 0)
-        {
-            //Invoke.Scenmanagement(HighScore);
-        }
         for (int i = 0; i < hearts.Length; i++)
         {
             if (i < playerHealth )
             {
                 hearts[i].enabled = true;
+                
             }
             else
             {
                 hearts[i].enabled = false;
             }
 
-        }
+        } 
        
     }
 
@@ -66,5 +73,10 @@ public class HealthAndDamageController : MonoBehaviour
         Time.timeScale = 0;
         yield return new WaitForSecondsRealtime(0.2f);
         Time.timeScale = 1;
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(2);
     }
 }
