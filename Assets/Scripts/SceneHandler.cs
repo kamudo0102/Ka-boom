5175387dbb15fcc1fdd2b29326718552e4ecd523
kamudo0102 <kamonphornudornrak@hotmail.com>
2023-09-21 07:53:06 +0200

    menu

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Scenehandler : MonoBehaviour
 {
+
+    [SerializeField] Image keyboard;
+    [SerializeField] Image mouse;
+
+    
     public void PlayGame()
     {
         SceneManager.LoadScene("SampleScene");
     }
+
+    //public void HowToPlay()
+    //{
+      
+    //}
 
     public void QuitGame()
     {
