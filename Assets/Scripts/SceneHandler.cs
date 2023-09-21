ad5d66e20aaa1e32857cd50d5be7f727f4f08a55
kamudo0102 <kamonphornudornrak@hotmail.com>
2023-09-21 09:13:56 +0200

    fixar image appear

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,16 +11,29 @@ public class Scenehandler : MonoBehaviour
     [SerializeField] Image keyboard;
     [SerializeField] Image mouse;
 
+    bool isImageDisplay = false;
     
+
+    
+
     public void PlayGame()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
-    //public void HowToPlay()
-    //{
-      
-    //}
+    public void DisplayImage()
+    {
+        if (isImageDisplay == false)
+        {
+            keyboard.enabled = true; mouse.enabled = true;
+            isImageDisplay = true;  
+        }
+        else if (isImageDisplay == true)
+        {
+            keyboard.enabled = false; mouse.enabled = false;
+            isImageDisplay = false;
+        }
+    }
 
     public void QuitGame()
     {
